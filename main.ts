7380a3a4ecc8b27bad37b8687feaeba3bde9df69
leejiho1531 <117968340+leejiho1531@users.noreply.github.com>
2022-11-11 14:53:54 +0900

    vbghghbj

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(320)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(270)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(150)
+})
+radio.setGroup(255)
+basic.showLeds(`
+    . . . . .
+    # # # . .
+    . . # . .
+    . . # # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.clearScreen()
+basic.forever(function () {
+    radio.setGroup(255)
+})
